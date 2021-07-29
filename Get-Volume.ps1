fe76be573ba498c1b95699c86803251a18f59197
Alex Bytes <bytesizedalex@users.noreply.github.com>
2021-07-29 20:01:40 +0100

    Create Get-Volume.ps1

---
+++ Get-Volume.ps1
@@ -0,0 +1,13 @@
+Get-Volume | Select-Object -Property DriveLetter,
+    FileSystemLabel,
+    FileSystem,
+    DriveType,
+    HealthStatus,
+    OperationalStatus,
+    AllocationUnitSize,
+    UniqueId,
+    @{Name='SizeGB'; Expression={"$([int]($_.Size / 1GB)) GB"}},
+    @{Name='SpaceUsed'; Expression={"$([int](($_.Size - $_.SizeRemaining) / 1GB)) GB"}},
+    @{Name='SizeRemainingGB'; Expression={"$([int]($_.SizeRemaining / 1GB)) GB"}},
+    @{Name='PercentFull'; Expression={"$([int](100 - (($_.SizeRemaining / $_.Size) * 100))) %"}},
+    @{Name='FreeSpacePercent'; Expression= {"$([int](($_.SizeRemaining / 1GB) / ($_.Size / 1GB) * 100)) %"} }
